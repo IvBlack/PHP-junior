8e24b0c06812f214cd2773e67595e2d5b7a3a7f7
IvBlack <ivbl21@ya.ru>
2021-09-11 00:18:04 +0300

    xmlparserservice rebuild

--- app/Services/XMLParserService.php
+++ app/Services/XMLParserService.php
@@ -6,13 +6,38 @@ use App\News;
 use Orchestra\Parser\Xml\Facade as XmlParser;
 use Illuminate\Support\Str;
 
+use Laravie\Parser\Xml\Reader;
+use Laravie\Parser\Xml\Document;
+
 
 class XMLParserService
 {
-    public function saveNews($link) {
-        $xml = XmlParser::load($link);
-        $dump($xml);
-        $data = $xml->parse([
+    public function saveNews($rssLink) {
+        //$rss = 'https:://news.yandex.ru/martial_arts.rss';
+        $xml = @simplexml_load_file($rssLink);
+        if($xml===false)die('Error connect to RSS, try again.');
+
+
+        foreach($xml->xpath('//item') as $item){
+            echo '<pre>';
+            echo ''.$item->title.'('.$item->pubDate.')';
+            echo '</br>';
+            echo ''.$item->description.'';
+            echo ''.$item->link.'';
+            echo '</pre>';
+        }
+
+        /*$xmlstr = @file_get_contents($link);
+        if($xmlstr===false)die('Error connect to URL: '.$xmlstr);
+
+        $xml = new SimpleXMLElement($xmlstr);
+        if($xml===false)die('Error connect to RSS: '.$xmlstr);
+        dump($xml);*/
+
+
+
+        //$xml = XmlParser::extract($link);
+        /*$data = $xml->parse([
         'title' => ['uses' => 'channel.title'],
         'link' => ['uses' => 'channel.link'],
         'description' => ['uses' => 'channel.description'],
@@ -20,9 +45,9 @@ class XMLParserService
         'news' => ['uses' => 'channel.item[guid,title,link,description,pubDate,enclosure::url,category]'],
     ]);
 
-    dump($data);
+    dump($data);*/
 
-        foreach ($data['news'] as $news) {
+        /*foreach ($data['news'] as $news) {
             if($news['category']) {
                 $category = Category::query()->firstOrCreate([
                     'name' => $news['category'],
@@ -37,6 +62,7 @@ class XMLParserService
                     'category_id' => $category->id,
                 ]);
             }
-        }
+        }*/
+        //return redirect()->route('news.category.index');
     }
 }
