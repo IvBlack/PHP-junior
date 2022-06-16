6a41f13f5ec5fa3475fdc38fdc6b6521849a091e
IvBlack <ivbl21@ya.ru>
2022-06-16 07:26:51 +0300

    blogpost project created

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', '\App\Http\Controllers\PostController@index');
+Route::get('post/', '\App\Http\Controllers\PostController@index')->name('post.index');
+Route::get('post/create', '\App\Http\Controllers\PostController@create')->name('post.create');
+Route::get('post/show/{id}', '\App\Http\Controllers\PostController@show')->name('post.show');
+Route::post('post/', '\App\Http\Controllers\PostController@store')->name('post.store');
